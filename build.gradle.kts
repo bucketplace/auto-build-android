465f8e44c1075397f5d53cc9eda04835969270ff
bsscco <bsscco@gmail.com>
2019-10-05 03:11:31 +0900

    gradle compile 함수를 implementation으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,17 +33,18 @@ repositories {
 }
 
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    compile("io.ktor:ktor-server-netty:$ktor_version")
-    compile("ch.qos.logback:logback-classic:$logback_version")
-    compile("io.ktor:ktor-server-core:$ktor_version")
-    compile("io.ktor:ktor-gson:$ktor_version")
-    compile("io.ktor:ktor-client-core:$ktor_version")
-    compile("io.ktor:ktor-client-core-jvm:$ktor_version")
-    compile("io.ktor:ktor-client-json-jvm:$ktor_version")
-    compile("io.ktor:ktor-client-gson:$ktor_version")
-    compile("io.ktor:ktor-client-okhttp:$ktor_version")
-    testCompile("io.ktor:ktor-server-tests:$ktor_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-gson:$ktor_version")
+    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    implementation("io.jsondb:jsondb-core:1.0.85")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
