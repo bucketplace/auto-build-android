ddbcea981491e5cffbf758c441aea73f0ac5cf15
bsscco <bsscco@gmail.com>
2019-10-05 03:10:18 +0900

    테스트 코드 오류 수정

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,17 +1,4 @@
-package bsscco
-
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
-import io.ktor.features.*
-import io.ktor.client.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 
